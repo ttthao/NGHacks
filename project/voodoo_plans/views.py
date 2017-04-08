e301673d64e1ee93f736fc25810cd5a3407defaa
John <JLSenar@gail.com>
2017-04-08 01:31:25 -0700

    Implemented distand and duration calculation for route

--- project/voodoo_plans/views.py
+++ project/voodoo_plans/views.py
@@ -6,6 +6,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
     return HttpResponse("Hello, world. You're at the voodoo_plans index.")
