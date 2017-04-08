6d194843205b1e9b082fc30ff36193d25c5e29b8
John <JLSenar@gail.com>
2017-04-08 05:55:31 -0700

    updated models

--- project/voodoo_plans/models.py
+++ project/voodoo_plans/models.py
@@ -3,4 +3,33 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Itinerary(models.Model):
+	title = models.CharField(max_length=200)
+	party = models.CharField(max_length=100)
+	occasion = models.CharField(max_length=200)
+	cost = models.IntegerField(default=0)
+	duration = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.title
+
+class Activity(models.Model):
+	title = models.CharField(max_length=200)
+	address = models.CharField(max_length=200)
+	rating = models.IntegerField(default=0)
+	image_url = models.URLField(max_length=100)
+
+	def __str__(self):
+		return self.title
+
+class Route(models.Model):
+	itinerary = models.ForeignKey(
+		Itinerary,
+		on_delete=models.CASCADE,
+		)
+	activity = models.ForeignKey(
+		Activity,
+		on_delete=models.CASCADE,
+		)
+	order = models.IntegerField(default=0)
 # Create your models here.
